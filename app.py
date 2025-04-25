dd8c796c44f179647a71632a7964387d67554247
Mrityunjay Pathak <themrityunjaypathak@gmail.com>
2025-04-25 17:31:48 +0530

    feat: create list of movies for application building

--- app.py
+++ app.py
@@ -14,3 +14,7 @@ sys.path.append(os.path.abspath("../utils"))
 # Page Configuration
 from utils.file_locator import get_path
 st.set_page_config(page_title='Pickify', page_icon=get_path('images', 'logo.png'), layout='wide')
+
+# List of Movies
+movies = pd.read_csv(get_path('clean_data','cleaned_data.csv'))
+movies_list = movies['title'].values
