e1d802e9ac2db1c952f9ecbd4dc50f7b0d017e53
Mrityunjay Pathak <themrityunjaypathak@gmail.com>
2025-04-25 17:17:46 +0530

    chore: set up streamlit page configuration

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ import streamlit as st
 
 # Adding utils directory to PYTHONPATH
 sys.path.append(os.path.abspath("../utils"))
+
+# Page Configuration
+from utils.file_locator import get_path
+st.set_page_config(page_title='Pickify', page_icon=get_path('images', 'logo.png'), layout='wide')
