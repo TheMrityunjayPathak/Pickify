2c39d3f4fe99a4769f8492ffc16d8e6ef164d562
Mrityunjay Pathak <themrityunjaypathak@gmail.com>
2025-05-19 13:39:51 +0530

    feat: download and load similarity matrix for recommendations

--- app.py
+++ app.py
@@ -18,3 +18,15 @@ st.set_page_config(page_title='Pickify', page_icon=get_path('images', 'logo.png'
 # List of Movies
 movies = pd.read_csv(get_path('clean_data','cleaned_data.csv'))
 movies_list = movies['title'].values
+
+# Downloading similarity matrix from Google Drive
+file_id = '1Zo0ayKcdOxyqNmUjU2niKAVbWl5S3Sol'
+output = 'similarity.pkl'
+url = f'https://drive.google.com/uc?id={file_id}'
+
+if not os.path.exists(output):
+    gdown.download(url, output, quiet=False)
+
+# Loading similarity matrix
+with open(output, 'rb') as f:
+    similarity = pickle.load(f)
