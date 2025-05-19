4ba8518d7b6c4e616eebbc7615e5eabc1e1a4f83
Mrityunjay Pathak <themrityunjaypathak@gmail.com>
2025-05-19 13:40:43 +0530

    chore: add navbar in streamlit python file

--- app.py
+++ app.py
@@ -30,3 +30,59 @@ if not os.path.exists(output):
 # Loading similarity matrix
 with open(output, 'rb') as f:
     similarity = pickle.load(f)
+
+# Navbar Section
+st.markdown("""
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Special+Gothic+Expanded+One&display=swap" rel="stylesheet">
+            
+    <style>
+        .navbar {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            color: white;
+            font-family: "Special Gothic Expanded One", sans-serif;
+        }
+            
+        .navbar .logo {
+            font-size: 28px;
+            font-weight: bold;
+            color: #FD3A84;
+            letter-spacing: 0.8px
+        }
+            
+        .navbar .nav-right button {
+            background-color: #020200;
+            padding: 5px 15px;
+            color: white;
+            cursor: pointer;
+            font-size: 15px;
+            margin: 4px;
+            border: 2px solid rgba(190, 190, 190);
+            border-radius: 100px;
+        }
+            
+        .navbar .nav-right .login {
+            background-color: #FD3A84;
+            padding: 5px 15px;
+            color: white;
+            cursor: pointer;
+            font-size: 15px;
+            margin: 4px;
+            border: 2px solid #FD3A84;
+            border-radius: 100px;
+        }
+    </style>
+
+    <div class="navbar">
+        <div class="logo">Pickify</div>
+        <div class="nav-right">
+            <form action="#">
+                <button>Sign up</button>
+                <button class='login'>Login</button>
+            </form>
+        </div>
+    </div>
+""", unsafe_allow_html=True)
